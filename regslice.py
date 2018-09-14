35aa91d8de32b00ad87d7a2a10a72e791cdc80a3
greatshuyi <greatshuyi@sina.com.cn>
2018-09-14 19:25:55 +0800

    Update regslice.py

--- regslice.py
+++ regslice.py
@@ -3,35 +3,22 @@
 General register slice containing PASS_SLICE, FORWARD_SLICE, REVERSE_SLICE, FULL_SLICE
 """
 
-from myhdl import enum, intbv, modbv
-from myhdl import Signal, always_comb, always_ff, instances
-from myhdl import block
+# from .hdl import enum, intbv, modbv
+# from .hdl import Signal, always_comb, always_ff, instances
+# from .hdl import block
+#
+# from .hdl import UBool, UInt, SInt
 
-
-def UBool(val=0):
-    assert val >= 0
-    return Signal(bool(val))
-
-
-def UInt(width, val=0):
-    assert val >= 0 and width > 0
-    return Signal(modbv(val)[width:])
-
-
-def SInt(width, val=0):
-    assert val >= 0 and width > 0
-    return Signal(intbv(val)[width:])
+from hdl import *
 
 
 @block
 def pass_slice(
-    clk,
-    rst_n,
     # source side
     svalid,
     sready,
     spayload,
-    # output side
+    # sink side
     dvalid,
     dready,
     dpayload,
@@ -55,7 +42,7 @@ def forward_slice(
     svalid,
     sready,
     spayload,
-    # output side
+    # sink side
     dvalid,
     dready,
     dpayload,
@@ -63,18 +50,20 @@ def forward_slice(
 ):
 
     pen = Signal(bool(0))
-    svalid_r = Signal(bool(0))
+
+    # as forward slice, register incoming valid on source side
+    svalid_ff = Signal(bool(0))
 
     @always_comb
     def _comb():
-        sready.next = dready or not svalid_r
-        dvalid.next = svalid_r
-        pen.next = (svalid and not svalid_r) or (svalid and svalid_r and dready)
+        sready.next = dready or not svalid_ff
+        dvalid.next = svalid_ff
+        pen.next = (svalid and not svalid_ff) or (svalid and svalid_ff and dready)
 
     @always_ff(clk.posedge, reset=rst_n)
     def _seq():
         if svalid or dready:
-            svalid_r.next = svalid
+            svalid_ff.next = svalid
 
         if pen:
             dpayload.next = spayload
@@ -109,7 +98,6 @@ def reverse_slice(
         # source side
         sready.next = not loaded
 
-
     @always_ff(clk.posedge, reset=rst_n)
     def _loaded():
         if (svalid and not loaded) or dready:
@@ -122,7 +110,7 @@ def reverse_slice(
             dpayload.next = spayload
 
         # register ready on source side
-        sready_r.next = dready or not
+        sready_r.next = dready or not loaded
 
     return instances()
 
@@ -143,48 +131,44 @@ def full_slice(
 ):
 
     # uses a small fsm to control load sequence
-    st = enum("IDLE", "ONE", "ALL")
-    nxt = enum()
-    
-    iready = Signal(bool(0))
-    bload = Signal(bool(0))
+    state_t = enum("IDLE", "ONE", "ALL")
+
+    st = Signal(state_t.IDLE)
+    nxt = Signal(state_t.IDLE)
+
+    iready = UBool(0)
+    bload = UBool(0)
     ovalid = UBool(0)
-    
-    pena = Signal(bool(0))
-    penb = Signal(bool(0))
+
+    pena = UBool(0)
+    penb = UBool(0)
 
     # two set registers used as ping-pong to enable back-to-back transfer
-    prega = Signal(intbv(0)[width:])
-    pregb = Signal(intbv(0)[width:])
+    prega = UInt(width, val=0)
+    pregb = UInt(width, val=0)
 
     # select signal between prega & pregb
-    sel = Signal(bool(0))
-
+    sel = UBool(0)
 
     # FSM
     @always_comb
     def _transition():
-        if st == t_state.IDLE:
-
-        elif st == t_state.ONE:
-
-        elif st == t_state.ALL:
-
+        if st == state_t.IDLE:
+            nxt.next = state_t.ONE
+        elif st == state_t.ONE:
+            nxt.next = state_t.ALL
+        elif st == state_t.ALL:
+            nxt.next = state_t.IDLE
         else:
             nxt.next = st
 
-
-    @always_ff
+    @always_ff(clk.posedge, reset=rst_n)
     def _state():
         st.next = nxt
 
-    @always_comb
-    def _comb():
-        pass
-
     @always_ff(clk.posedge, reset=rst_n)
     def _enable():
-        #all payload load enable sigs
+        # all payload load enable sigs
         pena.next = not bload and svalid and iready
         penb.next = bload and svalid and iready
 
@@ -192,25 +176,41 @@ def full_slice(
     def _payloada():
         if pena:
             prega.next = spayload
-            print("Loading payload into register A")
+            # print("Loading payload into register A")
 
     @always_ff(clk.posedge, reset=rst_n)
     def _payloadb():
         if penb:
             pregb.next = spayload
-            print("Loading payload into register B")
+            # print("Loading payload into register B")
 
     @always_comb
     def _out():
-        print("Selecting from register{}".format("B" if sel else "A"))
+        # print("Selecting from register{}".format("B" if sel else "A"))
         dpayload.next = prega if not sel else pregb
         sready.next = iready
         dvalid.next = ovalid
 
-
     return instances()
 
 
-
 if __name__ == "__main__":
-    pass
+
+    width = 16
+
+    clk = UBool(0)
+    rst_n = AsyncReset(1, 0)
+    svalid = UBool(0)
+    sready = UBool(0)
+    spayload = UInt(width)
+    dvalid = UBool(0)
+    dready = UBool(0)
+    dpayload = UInt(width)
+
+    fwd_slice = full_slice(clk, rst_n, svalid, sready, spayload,
+                              dvalid, dready, dpayload, width)
+
+    fwd_slice.convert(hdl="Verilog")
+
+
+
