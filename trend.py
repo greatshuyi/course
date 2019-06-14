9d5292b63a2b14d19a2f5e69f039342ef2a6e767
greatshuyi <greatshuyi@sina.com.cn>
2019-06-14 19:03:26 +0800

    Update trend.py

--- trend.py
+++ trend.py
@@ -48,3 +48,95 @@ def split_trend(df, index_col=None):
                 upl.append(trend)
             else:
                 dnl.append(trend)
+
+
+class Analyzer(object):
+    
+    def __call__(self, df):
+        self.df = df
+                
+class BaseAnalyzer(object):
+    
+    def first_max_by_column(self, df, cname):
+        """Return a tuple of (index, series) of the first maximum's location and row(as series),
+        """
+        idx = df[cname].idxmax()
+        row = df.loc[idx]
+        return idx, row
+    
+    def first_min_by_column(self, df, cname):
+        """Return a tuple of (index, series) of the first maximum's location and row(as series),
+        """
+        idx = df[cname].idxmax()
+        row = df.loc[idx]
+        return idx, row
+    
+    
+    def nlargest(self, df, cname):
+        """Return a tuple of (list, DataFrame) of the nlargests, list's element are indexes of
+        the founded row, df is just return by nlargest method
+        """
+        d = df.nlargest(cname)
+        idxes = d.index.to_list()
+        
+        return idxes, d
+        
+    
+    def nan_columns(self, df):
+        """Return a list of column's name contains nan value"""
+        d = df.isna().any().to_dict()
+        return [ k for k, v in d.items() if v is True]
+    
+    
+class QuoteFormater(object):
+
+    def ensure_base_column_name(df):
+        pass
+    
+    
+    
+GDK_COLUMN_NAME = 'gdk'
+
+
+
+
+
+
+class BaseReporter(object):
+    
+    def __call__(self, df):
+        self.df = df
+
+
+class GDKReporter(BaseReporter):
+    
+    
+    def report(self):
+        # gk analysis, 高开
+        gkdf = df[df[GDK_COLUMN_NAME] > 0]
+        
+        # dk analysis，低开
+        dkdf = df
+        
+    def nday_yield(self, ):
+        df = self.df
+        
+        
+
+
+class TrendAnalyzer(BaseAnalyzer):
+    
+    def updn(self):
+        
+        # 当天涨跌限制
+        df = self.df
+        df[UPDN_COLUMN_NAME] = df['Close'] - df['Open']
+    
+    def gdk(self):
+        df = self.df
+        
+        # 高低开的Gap
+        df[GDK_COLUMN_NAME] = df['Open'] - df['Preclose']
+        
+        # 高低开当天的涨跌Gap
+        self.updn()
