37e670d312f8116adda5f36c60f640bc601a1d4a
greatshuyi <greatshuyi@sina.com>
2019-09-02 02:38:55 +0800

    Update tlb_fsm.py

--- tlb_fsm.py
+++ tlb_fsm.py
@@ -8,12 +8,10 @@ from myhdl import ConcatSignal as concat
 from pipeline.hdl import *
 
 
-
 @block
 def pa_composer(va, prev_va,
                 utlb_va, utlb_pa, uutlb_page_size,
                 same_page, pa):
-
     @always_comb
     def composer():
 
@@ -31,60 +29,57 @@ def pa_composer(va, prev_va,
             pass
 
 
-
 @block
 def cross_page_check(va, utlb_va, utlb_page_size,
                      cross_page,
                      ):
-
-    same_4k   = UBool(0)
-    same_16k  = UBool(0)
-    same_64k  = UBool(0)
-    same_2m   = UBool(0)
-    same_32m  = UBool(0)
+    same_4k = UBool(0)
+    same_16k = UBool(0)
+    same_64k = UBool(0)
+    same_2m = UBool(0)
+    same_32m = UBool(0)
     same_512m = UBool(0)
-    same_1g   = UBool(0)
-    same_16g  = UBool(0)
-    page_4k   = UBool(0)
-    page_16k  = UBool(0)
-    page_64k  = UBool(0)
-    page_2m   = UBool(0)
-    page_32m  = UBool(0)
+    same_1g = UBool(0)
+    same_16g = UBool(0)
+    page_4k = UBool(0)
+    page_16k = UBool(0)
+    page_64k = UBool(0)
+    page_2m = UBool(0)
+    page_32m = UBool(0)
     page_512m = UBool(0)
-    page_1g   = UBool(0)
-    page_16g  = UBool(0)
+    page_1g = UBool(0)
+    page_16g = UBool(0)
 
     @always_comb
     def page_check():
-        same_4k.next   = True if va[48:12] == utlb_va[48:12] else False
-        same_16k.next  = True if va[48:14] == utlb_va[48:12] else False
-        same_64k.next  = True if va[48:16] == utlb_va[48:12] else False
-        same_2m.next   = True if va[48:21] == utlb_va[48:12] else False
-        same_32m.next  = True if va[48:25] == utlb_va[48:12] else False
+        same_4k.next = True if va[48:12] == utlb_va[48:12] else False
+        same_16k.next = True if va[48:14] == utlb_va[48:12] else False
+        same_64k.next = True if va[48:16] == utlb_va[48:12] else False
+        same_2m.next = True if va[48:21] == utlb_va[48:12] else False
+        same_32m.next = True if va[48:25] == utlb_va[48:12] else False
         same_512m.next = True if va[48:29] == utlb_va[48:12] else False
-        same_1g.next   = True if va[48:30] == utlb_va[48:12] else False
-        same_16g.next  = True if va[48:34] == utlb_va[48:34] else False
+        same_1g.next = True if va[48:30] == utlb_va[48:12] else False
+        same_16g.next = True if va[48:34] == utlb_va[48:34] else False
 
-        page_4k.next   = True if utlb_page_size == 0 else False
-        page_16k.next  = True if utlb_page_size == 1 else False
-        page_64k.next  = True if utlb_page_size == 2 else False
-        page_2m.next   = True if utlb_page_size == 3 else False
-        page_32m.next  = True if utlb_page_size == 4 else False
+        page_4k.next = True if utlb_page_size == 0 else False
+        page_16k.next = True if utlb_page_size == 1 else False
+        page_64k.next = True if utlb_page_size == 2 else False
+        page_2m.next = True if utlb_page_size == 3 else False
+        page_32m.next = True if utlb_page_size == 4 else False
         page_512m.next = True if utlb_page_size == 5 else False
-        page_1g.next   = True if utlb_page_size == 6 else False
-        page_16g.next  = True if utlb_page_size == 7 else False
+        page_1g.next = True if utlb_page_size == 6 else False
+        page_16g.next = True if utlb_page_size == 7 else False
 
-        cross_page.next = (page_4k   and same_4k)   or \
-                          (page_16k  and same_16k)  or \
-                          (page_64k  and same_64k)  or \
-                          (page_2m   and same_2m)   or \
-                          (page_32m  and same_32m)  or \
+        cross_page.next = (page_4k and same_4k) or \
+                          (page_16k and same_16k) or \
+                          (page_64k and same_64k) or \
+                          (page_2m and same_2m) or \
+                          (page_32m and same_32m) or \
                           (page_512m and same_512m) or \
-                          (page_1g   and same_1g)   or \
-                          (page_16g  and same_1g)
+                          (page_1g and same_1g) or \
+                          (page_16g and same_1g)
 
     return page_check()
-
 
 
 TLB_STATE = enum('IDLE', 'TLB', 'WAIT', 'SYNC')
@@ -94,8 +89,13 @@ TLB_STATE = enum('IDLE', 'TLB', 'WAIT', 'SYNC')
 def tlb_ctrl(
     clk, rst_n, enable, flush,
 
-    # upstream input
-    ivld, istart, idrop, iurgent, imode, ioffset, iva,
+    # pipeline input
+    # command
+    ivld, istart, idrop,
+    iurgent, imode, ibase, ioffset,
+
+    # ooo flush
+    ooo_flush,
 
     # from pipelien & data path
 
@@ -104,11 +104,8 @@ def tlb_ctrl(
 
 
 
-    # ooo flush
-    ooo_flush,
-
-    # pipeline output
-    ovalid, istall,
+    # downstream pipeline interface
+    istall, ovalid,
 
     # backpressure to previous pipe
     ostall,
@@ -116,31 +113,48 @@ def tlb_ctrl(
     # monitor
     st, nxt
 ):
-
     # =============================================
     # pipeline sigs and payload
     # =============================================
+
+    # staging buffer
+    stage_start = UBool(0)
+    stage_drop = UBool(0)
+    stage_base = UInt(len(ibase))
+    stage_offset = UInt(len(ioffset))
+
+    # mux-ed output
     tlb_start = UBool(0)
     tlb_drop = UBool(0)
-    
-    va = UInt(len(iva), 0)                          # current processing request's VA
-    pa = UInt(len(prf_pa), 0)
+    base = UInt(len(ibase))
+    offset = UInt(len(ioffset))
+
+    # clear sig from FSM to stage cmd
+    clr_start = UBool(0)
+    clr_drop = UBool(0)
 
     # act as uuTLB entry
     uutlb_vld = UBool(0)
-    uutlb_page_size=UInt(3)
+    uutlb_page_size = UInt(3)
     uutlb_va = UInt(len(iva), 0)
     uutlb_pa = UInt(len(pa), 0)
 
     st = UEnum(TLB_STATE)
     nxt = UEnum(TLB_STATE)
 
-    prf_sync = UBool(0)
-
     tlb_timer = UInt(9, max=511)
     tlb_timeout = UBool(0)
+    prf_sync = UBool(0)
 
+    # data path & pipeline
+    iva = UInt(len(ibase)+len(ioffset))
     cross_page = UBool(0)
+
+    # pipeline internal stall
+    stop = UBool(0)
+
+    va = UInt(len(iva), 0)  # current processing request's VA
+    pa = UInt(len(prf_pa), 0)
 
     # =============================================
     # pipeline control
@@ -148,11 +162,26 @@ def tlb_ctrl(
 
     @always_comb
     def glb_ctrl():
-        tlb_start.next = istart or ooo_flush
-        tlb_drop.next = ivld and idrop
+        tlb_start.next = stage_start if stop else istart
+        tlb_drop.next = stage_drop if stop else idrop
+        base.next = stage_base if stop else ibase
+        offset.next = stage_offset if stop else ioffset
+        # tlb_start.next = istart or ooo_flush
+        # tlb_drop.next = ivld and idrop
         tlb_timeout.next = True if st == TLB_STATE.WAIT and tlb_timer == 511 else False
         prf_sync.next = prf_vld and (not prf_fault and prf_match)
         cross_page.next = (iva == uutlb_va) and uutlb_vld
+
+    @always_ff(clk.posedge, reset=rst_n)
+    def staging():
+        if flush:
+            stage_start.next = False
+            stage_drop.next = False
+            stage_base.next = 0
+            stage_offset.next = 0
+        elif not stop and enable:
+            stage_base.next = ibase
+            stage_offset.next = ioffset
 
     @always_ff(clk.posedge, reset=rst_n)
     def fsm_state():
@@ -195,7 +224,7 @@ def tlb_ctrl(
                 nxt.next = TLB_STATE.TLB
             else:
                 nxt.next = TLB_STATE.TLB if cross_page else TLB_STATE.SYNC
-                
+
         else:
             nxt.next = TLB_STATE.IDLE
 
@@ -246,9 +275,9 @@ def tlb_ctrl(
 
         # page size/pa/...
         if enable and st == TLB_STATE.WAIT and prf_sync:
-                uutlb_pa.next = prf_pa
-                uutlb_page_size.next = prf_page_size
-        else:                                               # hold
+            uutlb_pa.next = prf_pa
+            uutlb_page_size.next = prf_page_size
+        else:  # hold
             uutlb_pa.next = uutlb_pa
             uutlb_page_size.next = uutlb_page_size
 
@@ -258,61 +287,35 @@ def tlb_ctrl(
 
     @always_ff(clk.posedge, reset=rst_n)
     def va_controls():
-        if flush:
-            va.next = 0
-        elif tlb_start or (tlb_drop and st != TLB_STATE.IDLE):
-            va.next = iva
-        else:
-            if st == TLB_STATE.WAIT:
-                pass
+
 
 
     @always_ff(clk.posedge, reset=rst_n)
     def pa_control():
-            if flush:
-                pa.next = 0
-            elif tlb_start:
-                pa.next = 0
-            else:
-                if st == TLB_STATE.WAIT and prf_sync:
-                    pa.next = prf_pa
-                elif st == TLB_STATE.SYNC:
-                    if tlb_drop:
-                        pass
+        if flush:
+
 
     @always_ff(clk.posedge, reset=rst_n)
     def handshake():
         # ovalid
-        if flush:
+        if flush or tlb_start:
             ovalid.next = False
-        elif tlb_start:
-            ovalid.next = False
+        elif st == TLB_STATE.WAIT:
+            ovalid.next = True if prf_sync else False
+        elif st == TLB_STATE.SYNC:
+            ovalid.next = False if cross_page or tlb_drop else True
         else:
-            pass
-
-
-
-
-
-
-
-
-
-
+            ovalid.next = ovalid
 
     # =============================================
-    # backpressure control
+    # back pressure control
     # =============================================
-    @always_ff(clk.posedge, reset=rst_n)
+    @always_comb
     def back_pressure():
-        if flush:
-            stall.next = False
-        elif tlb_start:
-            stall.next = True
+        if st == TLB_STATE.IDLE:
+            ostall.next = False
         else:
-            pass
-
-
+            ostall.next = not flush | stop | istall
 
     return instances()
 
