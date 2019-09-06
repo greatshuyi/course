612ee02eec8a9e432f4e5cdc43572de199c53397
greatshuyi <greatshuyi@sina.com>
2019-09-06 20:09:27 +0800

    Update pot_fsm.py

--- pot_fsm.py
+++ pot_fsm.py
@@ -6,112 +6,69 @@ from myhdl import (
 )
 from myhdl import ConcatSignal as concat
 from pipeline.hdl import *
-from myhdl import delay, StopSimulation
 
 
-from pipeline.lib.mem import ssrom
-
-
-from pipeline.sim.reciever import reciever as downstream
-
-
-
-
-
-@block
-def ram_decoder(cen, ren, index, offset,
-                bank_cen, bank_ren, bank_addr, bank_select
-                ):
-
-    addr = concat(index, offset)
-
-    @always_comb
-    def decoder():
-        bank_cen.next = cen
-        bank_ren.next = ren
-        bank_addr.next = addr
-        bank_select.next = 1
-
-
-ram_decoder.verilog_code = \
-"""
-ram_decoder ram_decoder (
-    .cen(cen),
-    .ren(ren),
-    .index(bank_idx),
-    .offset(bank_oft),
-    .bank_cen(bank_cen),
-    .bank_ren(bank_ren),
-    .bank_index(bank_index),
-    .bank_select(bank_select)
-    );
-"""
 
 POT_STATE = enum('IDLE', 'RD', 'PEND')
 
 
 @block
-def pot_fsm(clk, rst_n, enable,
-            # upstream input
-            flush, start, index, offset, amount, base, mode,
-            # combinatorial SRAM input/output control
-            bank_cen, bank_ren, bank_addr, bank_mask, bank_select,
-            # pipe cmd
-            ovld, ostart, odrop, ourgent, omode,
-            # pipe registers data
-            obase,
-            # pipe ctrl
-            cnt,
-            # reciever feedback
-            stall,
-            # monitor
-            st, nxt,
-            # for simulation only
-            rcen, rren, raddr
-            ):
-
+def pot_ctrl(clk, rst_n, enable,
+             iflush,
+             istart,
+             iindex,
+             icolumn,
+             iamount,
+             ibase,
+             imode,
+             iprf_mode,
+             itid,
+             ram_ren,
+             ram_index,
+             ram_offset,
+             ostart,
+             odrop,
+             omode,
+             obase,
+             otid,
+             cnt,
+             istall,
+             oack,
+             st,
+             nxt,
+             rcen,
+             rren,
+             raddr
+             ):
     # =============================================
     # Constant & Parameter
     # =============================================
     MAX_OFFSET = 14
     MAX_INDEX = 64
-    MAX_TIMER = 4
+    OFFSET_LIMIT = MAX_OFFSET-1
+    INDEX_LIMIT = MAX_INDEX-1
+
+    MAX_TIMER = 1024
 
     # =============================================
     # Sigs and pipe registers
     # =============================================
 
-    ovld = UBool(0)                         # pipe command, ovld act as valid
-    ostart = UBool(0)                       # pipe command, reciever restart
-    odrop = UBool(0)                        # pipe command, reciever drop current
-    ourgent = UBool(0)                      # pipe QoS for reciever
+    cnt = UInt(len(iamount))  # pipe register of amount counter
+    idx = UInt(len(iindex))  # pipe register of index
+    oft = UInt(len(icolumn))  # pipe register of offset
 
-    ocomplete = UBool(0)                    # to main control and upstream
+    nxt_cnt = UInt(len(iamount))
+    nxt_idx = UInt(len(iindex))
+    nxt_oft = UInt(len(icolumn))
+    nxt_idx = UInt(len(iindex))
+    nxt_cnt = UInt(len(iamount))
 
-    omode = UInt(2, 0)
-
-    cnt = UInt(len(amount))                 # pipe register of amount counter
-    idx = UInt(len(index))                  # pipe register of index
-    oft = UInt(len(offset))                 # pipe register of offset
-
-    nxt_cnt = UInt(len(amount))
-    nxt_idx = UInt(len(index))
-    nxt_oft = UInt(len(offset))
-    nxt_idx = UInt(len(index))
-    nxt_cnt = UInt(len(amount))
-
-    timer = UInt(11, 1024)                  # pending time out timer
+    timer = UInt(11)  # pending time out timer
 
     pot_start = UBool(0)
-    pot_complete = UBool(0)                 # complete all transfer or reach POT last column
+    pot_complete = UBool(0)  # complete all transfer or reach POT last column
     pot_timeout = UBool(0)
-    pot_enable = UBool(0)
-
-    # internal RAM control
-    cen = UBool(0)
-    ren = UBool(0)
-    bank_idx = UInt(len(idx))
-    bank_oft = UInt(len(oft))
 
     # =============================================
     # pipeline control
@@ -120,9 +77,8 @@ def pot_fsm(clk, rst_n, enable,
     # FSM start condition
     @always_comb
     def fsm_start():
-        pot_start.next = start and amount != 0
-        pot_complete.next = nxt_cnt == 0 or (idx == MAX_INDEX) and (oft == MAX_OFFSET)
-        pot_enable.next = enable and not stall
+        pot_start.next = istart and iamount != 0
+        pot_complete.next = nxt_cnt == 0 or (idx == INDEX_LIMIT) and (oft == OFFSET_LIMIT)
         pot_timeout.next = True if timer == MAX_TIMER else False
 
     # pipe FSM
@@ -130,47 +86,50 @@ def pot_fsm(clk, rst_n, enable,
     def fsm_state():
         if enable:
             st.next = nxt
+        else:
+            st.next = st
 
     @always_comb
     def fsm_transition():
 
         if st == POT_STATE.IDLE:
-            if flush:
+            if iflush:
                 nxt.next = POT_STATE.IDLE
-            elif pot_start:
-                nxt.next = POT_STATE.RD
             else:
-                nxt.next = POT_STATE.IDLE
+                nxt.next = POT_STATE.RD if pot_start else POT_STATE.IDLE
 
         elif st == POT_STATE.RD:
-            if flush:
+            if iflush:
                 nxt.next = POT_STATE.IDLE
             elif pot_start:
                 nxt.next = POT_STATE.RD
             else:
-                if stall:
+                if istall:
                     nxt.next = POT_STATE.PEND
                 else:
-                    if pot_complete:
-                        nxt.next = POT_STATE.IDLE
-                    else:
-                        nxt.next = POT_STATE.RD
+                    nxt.next = POT_STATE.IDLE if pot_complete else POT_STATE.RD
 
         elif st == POT_STATE.PEND:
-            if flush:
+            if iflush:
                 nxt.next = POT_STATE.IDLE
-            elif pot_start:
-                nxt.next = POT_STATE.RD
             else:
-                if not stall:
-                    nxt.next = POT_STATE.PEND
-                else:
-                    if pot_timeout:
-                        nxt.next = POT_STATE.RD
-                    else:
-                        nxt.next = POT_STATE.PEND
+                nxt.next = POT_STATE.RD if pot_start or pot_timeout or not istall else POT_STATE.PEND
+
         else:
             nxt.next = POT_STATE.IDLE
+
+    # =============================================
+    # pipeline monitor
+    # =============================================
+    @always_ff(clk.posedge, reset=rst_n)
+    def pipe_counter():
+        if iflush or pot_start:
+            timer.next = 0
+        else:
+            if (st == POT_STATE.RD or st == POT_STATE.PEND) and istall:
+                timer.next = 0 if timer == MAX_TIMER else timer + 1
+            else:
+                timer.next = 0
 
     # =============================================
     # pipeline payload/register control
@@ -178,186 +137,187 @@ def pot_fsm(clk, rst_n, enable,
 
     @always_ff(clk.posedge, reset=rst_n)
     def pipe_registers():
-        if pot_enable:
+        if enable:
             idx.next = nxt_idx
             oft.next = nxt_oft
             cnt.next = nxt_cnt
 
     @always_comb
-    def pipe_control():
-        if flush:
+    def _cnt_ctrl():
+        if iflush:
             nxt_cnt.next = 0
-        elif pot_start:
-            nxt_cnt.next = amount
         else:
-            if not stall:
+            if pot_start:
+                nxt_cnt.next = iamount
+            elif not istall:
                 nxt_cnt.next = cnt - 1 if cnt != 0 else 0
             else:
                 nxt_cnt.next = cnt
 
-        if flush:
+    @always_comb
+    def _oft_ctrl():
+        if iflush:
             nxt_oft.next = 0
-        elif pot_start:
-            nxt_oft.next = offset
         else:
-            if not stall:
-                nxt_oft.next = 0 if oft == MAX_OFFSET else oft + 1
+            if pot_start:
+                nxt_oft.next = icolumn
+            elif not istall and idx != INDEX_LIMIT:
+                nxt_oft.next = 0 if oft == OFFSET_LIMIT else oft + 1
             else:
                 nxt_oft.next = idx
 
-        if flush:
+    @always_comb
+    def _idx_ctrl():
+        if iflush:
             nxt_idx.next = 0
-        elif pot_start:
-            nxt_idx.next = index
-        else:
-            if not stall:
-                if idx == MAX_INDEX:
-                    nxt_idx.next = MAX_INDEX
-                else:
-                    nxt_idx.next = idx + 1 if oft == MAX_OFFSET else idx
-            else:
-                nxt_idx.next = idx
-                       
-    @always_ff(clk.posedge, reset=rst_n)
-    def base_control():
-        if flush:
-            obase.next = 0
         else:
             if pot_start:
-                obase.next = base
+                nxt_idx.next = iindex
+            elif not istall and idx != INDEX_LIMIT and oft != OFFSET_LIMIT:
+                    nxt_idx.next = idx + 1
             else:
-                obase.next = obase
+                nxt_idx.next = idx
+
+    @always_ff(clk.posedge, reset=rst_n)
+    def start_ctrl():
+        if iflush or not enable:
+            ostart.next = False
+        else:
+            ostart.next = istart
+
+    @always_ff(clk.posedge, reset=rst_n)
+    def drop_ctrl():
+        if iflush:
+            odrop.next = False
+        else:
+            odrop.next = True if (st == POT_STATE.WAI and pot_timeout and enable) else False
+
+    @always_ff(clk.posedge, reset=rst_n)
+    def base_control():
+        if iflush:
+            obase.next = 0
+        else:
+            obase.next = ibase if pot_start and enable else obase
 
     @always_ff(clk.posedge, reset=rst_n)
     def mode_control():
-        if flush:
+        if iflush:
             omode.next = 0
         else:
-            if pot_start:
-                omode.next = mode
-            else:
-                omode.next = omode
+            omode.next = imode if pot_start and enable else omode
+
+    @always_ff(clk.posedge, reset=rst_n)
+    def tid_ctrl():
+        if iflush:
+            otid.next = False
+        else:
+            otid.next = itid if pot_start and enable else otid
 
     # =============================================
-    # pipeline command controls
+    # command acknowledge
     # =============================================
 
-    # handshake valid
     @always_ff(clk.posedge, reset=rst_n)
-    def valid():
-        if flush:
-            ovld.next = False
+    def oack_ctrl():
+        if iflush or pot_start:
+            oack.next = False
         else:
-            if st == POT_STATE.IDLE and pot_start or st != POT_STATE.IDLE:
-                ovld.next = True
-            else:
-                ovld.next = False
-
-    @always_ff(clk.posedge, reset=rst_n)
-    def start_control():
-        if flush:
-            ostart.next = False
-        elif pot_start:
-            ostart.next = True
-        else:
-            if stall:
-                ostart.next = ostart
-            else:
-                ostart.next = False if ostart else ostart
-
-    @always_ff(clk.posedge, reset=rst_n)
-    def drop_control():
-        if flush:
-            odrop.next = False
-        elif start:
-            ostart.next = False
-        else:
-            if stall:
-                odrop.next = odrop
-            else:
-                odrop.next = True if st == POT_STATE.PEND and pot_timeout else False
-
-    @always_ff(clk.posedge, reset=rst_n)
-    def urgent_control():
-        if flush:
-            ourgent.next = False
-        elif pot_start:
-            ourgent.next = False
-        else:
-            if st == POT_STATE.IDLE and pot_start:
-                ourgent.next = True
-            else:
-                ourgent.next = ourgent
-
-    @always_ff(clk.posedge, reset=rst_n)
-    def complete_control():
-        if flush:
-            ocomplete.next = False
-        elif pot_start:
-            ocomplete.next = False
-        else:
-            if st == POT_STATE.RD and pot_complete:
-                ocomplete.next = True
-            else:
-                ocomplete.next = False
+            oack.next = True if not enable or (not oack and st == POT_STATE.RD and pot_complete) else False
 
     # =============================================
     # SRAM controls
     # =============================================
 
-    decoder = ram_decoder(cen=cen, ren=ren, index=bank_idx, offset=bank_oft,
-                          bank_cen=bank_cen, bank_ren=bank_ren,
-                          bank_addr=bank_addr, bank_mask=bank_mask,
-                          bank_sel=bank_select)
-
     @always_comb
     def ram_control():
-        if flush:
-            cen.next = False
-            ren.next = False
-        else:
-            if pot_start and st == POT_STATE.IDLE or st != POT_STATE.IDLE:
-                cen.next = True
-                ren.next = True
-            else:
-                cen.next = False
-                ren.next = False
-
-        bank_idx.next = nxt_idx
-        bank_oft.next = nxt_oft
-
-    # =============================================
-    # pipeline monitor
-    # =============================================
-    @always_ff(clk.posedge, reset=rst_n)
-    def pipe_counter():
-        if flush:
-            timer.next = 0
-        elif pot_start:
-            timer.next = 0
-        else:
-            if st == POT_STATE.RD and stall:
-                timer.next = timer + 1
-            elif st == POT_STATE.PEND:
-                if not stall:
-                    timer.next = 0
-                else:
-                    if pot_timeout:
-                        timer.next = 0
-                    else:
-                        timer.next = timer + 1
-            else:
-                timer.next = timer
+        ram_ren.next = True if not iflush and (pot_start and st == POT_STATE.IDLE or st == POT_STATE.RD) else False
+        ram_index.next = iindex if pot_start else idx
+        ram_offset.next = icolumn if pot_start else oft
 
     if __debug__:
         @always_comb
         def ram_sim():
-            rcen.next = cen
-            rren.next = ren
-            raddr.next = concat(bank_idx, bank_oft)
-
+            rcen.next = ram_ren
+            rren.next = ram_ren
+            raddr.next = concat(ram_index, ram_offset)
 
     return instances()
+
+
+def convert():
+    clk = Clock(0)
+    rst_n = AsyncReset(0)
+
+    BASE_VA_WIDTH = 43      # [48:6]
+    INDEX_WIDTH = 6         # [60:55]
+    COLUMN_WIDTH = 4        # [54:51]
+    AMOUNT_WIDTH = 7        # [50:44]
+
+    # wires
+    enable=UBool(0)
+    iflush = UBool(0)
+    istart = UBool(0)
+    ibase = UInt(BASE_VA_WIDTH)
+    iindex = UInt(INDEX_WIDTH)
+    icolumn = UInt(COLUMN_WIDTH)
+    iamount = UInt(AMOUNT_WIDTH)
+    imode = UInt(2)
+
+    iprf_mode = UInt(2)
+    itid = UBool(0)
+    ram_ren = UBool(0)
+    ram_index = UInt(INDEX_WIDTH)
+    ram_offset = UInt(COLUMN_WIDTH)
+    ostart = UBool(0)
+    odrop = UBool(0)
+    omode = UInt(2)
+    obase = UInt(BASE_VA_WIDTH)
+    otid = UBool(0)
+    cnt = UInt(AMOUNT_WIDTH)
+    istall = UBool(0)
+    oack = UBool(0)
+    st = UEnum(POT_STATE)
+    nxt = UEnum(POT_STATE),
+    rcen = UBool(0)
+    rren = UBool(0)
+    raddr = UInt(INDEX_WIDTH+COLUMN_WIDTH)
+
+    # Instance
+    # OFFSET_TABLE = [i + 1 for i in range(2 ** (INDEX_WIDTH+COLUMN_WIDTH))]
+
+    # ram = ssrom(clk=clk, ren=rren, addr=raddr,
+    #             dout=pipe_offset, CONTENT=OFFSET_TABLE)
+
+    pot = pot_ctrl(clk=clk, rst_n=rst_n, enable=enable,
+                   iflush=iflush,
+                   istart=istart,
+                   iindex=iindex,
+                   icolumn=icolumn,
+                   iamount=iamount,
+                   ibase=ibase,
+                   imode=imode,
+                   iprf_mode=iprf_mode,
+                   itid=itid,
+                   ram_ren=ram_ren,
+                   ram_index=ram_index,
+                   ram_offset=ram_offset,
+                   ostart=ostart,
+                   odrop=odrop,
+                   omode=omode,
+                   obase=obase,
+                   otid=otid,
+                   cnt=cnt,
+                   istall=istall,
+                   oack=oack,
+                   st=st,
+                   nxt=nxt,
+                   rcen=rcen,
+                   rren=rren,
+                   raddr=raddr
+                   )
+
+    pot.convert(hdl='Verilog')
+
 
 
 if __name__ == "__main__":
